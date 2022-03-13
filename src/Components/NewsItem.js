82f57e8aca16a6826cca99c653bc7117340091f6
ashishbhungani97 <bhunganiashish@gmail.com>
2022-03-13 15:21:30 +0530

    newsitems

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
    
     render() {
-        console.log('111');
-        console.log('222222222');
+        
+       
         let { title, disc, imgurl, newsUrl } = this.props
         return (
             <div className="card my-3" style={{ width: '18rem' }}>
